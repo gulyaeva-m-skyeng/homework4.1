f489bcd146208d08abfefaba9a488f3f923dc52b
gulyaeva-m-skyeng <marucosmos0924@gmail.com>
2024-11-23 12:15:28 +0300

    выполнение дз 2

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,16 @@ public class Main {
 
 //task2
         System.out.println("Task 2");
-for (int i = 10; i >= 1; i--) {
-    System.out.println(i + " ");
-}
-}
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i + " ");
+        }
+
+//task3
+        System.out.println("Task 3");
+        for (int i = 0; i < 17; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " ");
+            }
+        }
+    }
 }
